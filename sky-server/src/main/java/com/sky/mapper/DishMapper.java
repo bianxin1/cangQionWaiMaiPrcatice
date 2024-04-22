41fe99bceb8156f0c07794e43bdba78b67bfbbed
bianxin <2835212297@qq.com>
2024-04-22 16:27:34 +0800

    微信小程序客户端基础功能

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -10,6 +10,8 @@ import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.util.List;
+
 @Mapper
 public interface DishMapper {
 
@@ -55,4 +57,6 @@ public interface DishMapper {
      * @param dish
      */
     void update(Dish dish);
+
+    List<Dish> list(Dish dish);
 }
